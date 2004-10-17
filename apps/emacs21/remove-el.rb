67bfe0c80fb9f91675088a6ecac4c5f0cef9c736
tmurakam <tmurakam@ac2a9469-1e22-0410-b9a8-b176a6ac6125>
2004-10-17 07:03:07 +0000

    misc fix
    git-svn-id: http://svn.osdn.jp/svnroot/xqt/xqt2/trunk@784 ac2a9469-1e22-0410-b9a8-b176a6ac6125

--- apps/emacs21/remove-el.rb
+++ apps/emacs21/remove-el.rb
@@ -2,10 +2,11 @@
 
 # .elc が存在する場合、対応する .el を削除する
 
-filelist = `find #{ARGV[0]} -name "*.el" -print`
-filelist.split(/\n/).each do |file|
-	compiled = file.sub(/\.el$/, ".elc")
-	if (FileTest.exists?(compiled))
-		File.unlink(file)
-	end
+`find #{ARGV[0]} -name "*.el" -print`.each do |file|
+    file.chop!
+    compiled = file.sub(/\.el$/, ".elc")
+    if (FileTest.exists?(compiled))
+	puts "Remove #{file}"
+	File.unlink(file)
+    end
 end	
